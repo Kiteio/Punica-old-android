c3ca545669fc2c6cbe7b19e9bfc5ca239424fea2
Chanwing Chow <1877145464@qq.com>
2024-06-15 11:35:17 +0800

    初始

---
+++ app/src/main/java/org/kiteio/punica/MainActivity.kt
@@ -0,0 +1,29 @@
+package org.kiteio.punica
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.kiteio.punica.ui.theme.PunicaTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PunicaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text("Hello World", modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
