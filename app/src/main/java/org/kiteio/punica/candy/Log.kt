533687bf5a694751334182d059db882105e69966
Chanwing Chow <1877145464@qq.com>
2024-06-18 23:29:37 +0800

    日志

---
+++ app/src/main/java/org/kiteio/punica/candy/Log.kt
@@ -0,0 +1,34 @@
+package org.kiteio.punica.candy
+
+import android.util.Log
+
+/**
+ * 日志
+ */
+object Log {
+    private const val TAG = "Punica"
+
+
+    /**
+     * Debug 日志
+     * @param any
+     * @return [Int]
+     */
+    fun d(vararg any: Any) = Log.d(TAG, any.joinToString())
+
+
+    /**
+     * Info 日志
+     * @param any
+     * @return [Int]
+     */
+    fun i(vararg any: Any) = Log.i(TAG, any.joinToString())
+
+
+    /**
+     * Error 日志
+     * @param any
+     * @return [Int]
+     */
+    fun e(vararg any: Any) = Log.e(TAG, any.joinToString())
+}
