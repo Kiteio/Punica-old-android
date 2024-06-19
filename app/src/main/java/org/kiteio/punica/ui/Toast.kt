1c1fd8409013f4ba7c87f207187c4bdf31513fbb
ZhouKt <1877145464@qq.com>
2024-06-19 18:35:14 +0800

    Toast

---
+++ app/src/main/java/org/kiteio/punica/ui/Toast.kt
@@ -0,0 +1,24 @@
+package org.kiteio.punica.ui
+
+import android.widget.Toast
+import androidx.annotation.StringRes
+import org.kiteio.punica.AppContext
+
+/**
+ * [Toast]
+ * @param text
+ * @param duration
+ * @return [Toast]
+ */
+fun Toast(text: String, duration: Int = Toast.LENGTH_SHORT): Toast =
+    Toast.makeText(AppContext, text, duration)
+
+
+/**
+ * [Toast]
+ * @param resId
+ * @param duration
+ * @return [Toast]
+ */
+fun Toast(@StringRes resId: Int, duration: Int = Toast.LENGTH_SHORT): Toast =
+    Toast.makeText(AppContext, resId, duration)
