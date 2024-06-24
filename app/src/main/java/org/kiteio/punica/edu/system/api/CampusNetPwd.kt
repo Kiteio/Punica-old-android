1e55a2df0e6f5dab6be1d6f16a30bd9b1b3fb15f
Chanwing Chow <1877145464@qq.com>
2024-06-24 17:11:28 +0800

    校园网

---
+++ app/src/main/java/org/kiteio/punica/edu/system/api/CampusNetPwd.kt
@@ -0,0 +1,21 @@
+package org.kiteio.punica.edu.system.api
+
+import com.fleeksoft.ksoup.Ksoup
+import io.ktor.client.statement.bodyAsText
+import org.kiteio.punica.candy.route
+import org.kiteio.punica.edu.system.EduSystem
+
+/**
+ * 校园网默认密码
+ * @receiver [EduSystem]
+ * @return [String]
+ */
+suspend fun EduSystem.campusNetPwd(): String {
+    val text = session.fetch(EduSystem.route { CARD }).bodyAsText()
+
+    val document = Ksoup.parse(text)
+    val table = document.getElementById("xjkpTable")!!
+
+    return table.getElementsByTag("tr")[43].getElementsByTag("td")[3]
+        .text().substring(10)
+}
