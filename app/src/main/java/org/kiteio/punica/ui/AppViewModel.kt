8e0688373dc009e0308c1deddb0e2d0249bd5cd4
Chanwing Chow <1877145464@qq.com>
2024-08-28 22:48:54 +0800

    开学日期仅本地数据无效时更新

--- app/src/main/java/org/kiteio/punica/ui/AppViewModel.kt
+++ app/src/main/java/org/kiteio/punica/ui/AppViewModel.kt
@@ -10,16 +10,19 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.cancellable
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import org.kiteio.punica.candy.LocalDate
 import org.kiteio.punica.candy.catching
 import org.kiteio.punica.datastore.Keys
 import org.kiteio.punica.datastore.Preferences
 import org.kiteio.punica.datastore.Users
 import org.kiteio.punica.datastore.set
 import org.kiteio.punica.edu.WebVPN
+import org.kiteio.punica.edu.foundation.Semester
 import org.kiteio.punica.edu.foundation.User
 import org.kiteio.punica.edu.system.EduSystem
 import org.kiteio.punica.edu.system.api.campusNetPwd
 import org.kiteio.punica.edu.system.api.schoolStart
+import java.time.LocalDate
 
 /**
  * 应用级 [ViewModel]
@@ -54,7 +57,16 @@ class AppViewModel : ViewModel() {
             Users.edit { it.set(user) }
             Preferences.edit {
                 it[Keys.lastUsername] = user.name
-                catching { it[Keys.schoolStart] = eduSystem.schoolStart().toString() }
+
+                val old =
+                    it[Keys.schoolStart]?.let { schoolStart -> LocalDate.parse(schoolStart) }
+
+                // 在 本地无开学日期 或 旧日期为旧学期 时更新
+                if (old == null || !(old `in` EduSystem.semester)) catching {
+                    eduSystem.schoolStart()?.run {
+                        it[Keys.schoolStart] = this@run.toString()
+                    }
+                }
             }
 
             emit(eduSystem)
@@ -69,4 +81,21 @@ class AppViewModel : ViewModel() {
         eduSystem?.logout()
         eduSystem = null
     }
+}
+
+
+/**
+ * 判断 [LocalDate] 是否在 [semester] 代表的学期区间内
+ * @receiver [LocalDate]
+ * @param semester
+ * @return [Boolean]
+ */
+private infix fun LocalDate.`in`(semester: Semester) = when (year) {
+    // 学年开始
+    semester.year -> semester.term == 1 && monthValue in 8..12
+    // 学年结束，其中 1 月属于第一学期
+    semester.year + 1 -> semester.term == 2 && monthValue in 2..7 ||
+            semester.term == 1 && monthValue == 1
+
+    else -> false
 }
