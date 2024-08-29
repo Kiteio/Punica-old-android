bad7816a61ac4683e59c21c21b56e0f319d88ab1
Chanwing Chow <1877145464@qq.com>
2024-08-29 11:17:48 +0800

    新增佛山校区紧急电话

--- app/src/main/java/org/kiteio/punica/edu/EmergencyCall.kt
+++ app/src/main/java/org/kiteio/punica/edu/EmergencyCall.kt
@@ -15,34 +15,68 @@ sealed class EmergencyCall(
     val phoneNumber: String,
     val workingHours: () -> String
 ) {
-    data object Clinic: EmergencyCall(
-        R.string.clinic,
-        "13112234297",
-        { getString(R.string.number_of_hours, 24) }
-    )
+    /** 广州校区 */
+    sealed class Canton(
+        @StringRes nameResId: Int,
+        phoneNumber: String,
+        workingHours: () -> String
+    ) : EmergencyCall(nameResId, phoneNumber, workingHours) {
+        /** 门诊部 */
+        data object Clinic : EmergencyCall(
+            R.string.clinic,
+            "13112234297",
+            { getString(R.string.number_of_hours, 24) }
+        )
 
-    /** 校园报警 */
-    data object CampusAlarm : EmergencyCall(
-        R.string.campus_alarm,
-        "020-84096060",
-        { getString(R.string.number_of_hours, 24) }
-    )
+        /** 校园报警 */
+        data object CampusAlarm : Canton(
+            R.string.campus_alarm,
+            "020-84096060",
+            { getString(R.string.number_of_hours, 24) }
+        )
 
-    /** 校园警务室 */
-    data object CampusPoliceOffice : EmergencyCall(
-        R.string.campus_police_office,
-        "020-84096110",
-        { "8:30 - 17:30" }
-    )
+        /** 校园警务室 */
+        data object CampusPoliceOffice : Canton(
+            R.string.campus_police_office,
+            "020-84096110",
+            { "8:30 - 17:30" }
+        )
 
-    /** 官洲派出所 */
-    data object GuanzhouPoliceOffice : EmergencyCall(
-        R.string.guanzhou_police_station,
-        "020-84092782",
-        { getString(R.string.number_of_hours, 24) }
-    )
+        /** 官洲派出所 */
+        data object GuanzhouPoliceOffice : Canton(
+            R.string.guanzhou_police_station,
+            "020-84092782",
+            { getString(R.string.number_of_hours, 24) }
+        )
 
-    companion object {
-        val values by lazy { listOf(Clinic, CampusAlarm, CampusPoliceOffice, GuanzhouPoliceOffice) }
+        companion object {
+            val values by lazy { listOf(Clinic, CampusAlarm, CampusPoliceOffice, GuanzhouPoliceOffice) }
+        }
+    }
+
+
+    /** 佛山校区 */
+    sealed class Foshan(
+        @StringRes nameResId: Int,
+        phoneNumber: String,
+        workingHours: () -> String
+    ) : EmergencyCall(nameResId, phoneNumber, workingHours) {
+        /** 门诊部 */
+        data object Clinic : Foshan(
+            R.string.clinic,
+            "18566890063",
+            { getString(R.string.number_of_hours, 24) }
+        )
+
+        /** 校园报警 */
+        data object CampusAlarm : Foshan(
+            R.string.campus_alarm,
+            "0757-87828110",
+            { getString(R.string.number_of_hours, 24) }
+        )
+
+        companion object {
+            val values by lazy { listOf(Clinic, CampusAlarm) }
+        }
     }
 }
